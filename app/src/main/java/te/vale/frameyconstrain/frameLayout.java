2b701fea792f19dec51ad9853c979d086cf3f68b
Noel Vallejos <noel.72712510.va@gmail.com>
2022-08-31 12:08:29 -0400

    Frame LAYOUT

---
+++ app/src/main/java/te/vale/frameyconstrain/frameLayout.java
@@ -0,0 +1,14 @@
+package te.vale.frameyconstrain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class frameLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_frame_layout);
+    }
+}
